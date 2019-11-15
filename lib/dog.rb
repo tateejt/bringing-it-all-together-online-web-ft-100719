24d47625dc4a4fc646d4cbc3f9bcdf54053f3324
Emery Tate <tateejt@gmail.com>
2019-11-15 20:49:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,4 +6,15 @@ class Dog
     @name = name
     @breed = breed
   end
+  
+  def create_table
+    sql = <<-SQL
+      CREATE TABLE IF EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT, 
+        breed TEXT
+      )
+      SQL
+      
+  end
 end
